00464deb6c1516cd5c00ba63f600df6f4bb5c6f7
sewy210 <96600134+sewy210@users.noreply.github.com>
2022-02-22 19:57:13 +0100

    Recorrer el Arrays

--- js/Arrays.js
+++ js/Arrays.js
@@ -1,4 +1,4 @@
-var lista = ["fresa","chocolate","vainilla"]
+var lista = ["fresa","chocolate","vainilla", "menta", "limon"]
 // 
 //console.log(lista)
 //console.log(lista.length)
@@ -6,12 +6,9 @@ var lista = ["fresa","chocolate","vainilla"]
 //console.log(lista[1])
 //console.log(lista[2])
 let indice = 0
-let seguirEjecutando = true
-while (indice < 3 ){
+while (indice < lista.length ){
     console.log(lista[indice])
-    indice = indice + 1
-    console.log(lista[indice])
-    indice = indice + 1
-    console.log(lista[indice])
+    // Es lo mismo indice = indice + 1 que  indice += 1
+    indice += 1
 
 }
