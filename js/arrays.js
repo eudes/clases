cf5a2113bd31c29700c99ed6550e258067330d56
Alberto21700 <alberto_moreno11@hotmail.es>
2022-02-22 19:00:39 +0100

    actualizar cambios

---
+++ js/arrays.js
@@ -0,0 +1,16 @@
+var lista = ["fresa","chocolate","vainilla"]
+// console.log(lista)
+// console.log(lista.length)
+// console.log(lista[0])
+// console.log(lista[1])
+// console.log(lista[2])
+var indice = 0
+var seguirEjecutando = true 
+while (indice < 3){
+    console.log(lista[indice])
+    indice = indice + 1
+    console.log(lista[indice])
+    indice = indice + 1
+    console.log(lista[indice])
+    
+}
