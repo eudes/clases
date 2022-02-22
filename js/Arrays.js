27873e2372445b300f630e0b94dbe90f06ce65c1
sewy210 <96600134+sewy210@users.noreply.github.com>
2022-02-22 19:03:52 +0100

    Argumentos y listas

---
+++ js/Arrays.js
@@ -0,0 +1,17 @@
+var lista = ["fresa","chocolate","vainilla"]
+// 
+//console.log(lista)
+//console.log(lista.length)
+//console.log(lista[0])
+//console.log(lista[1])
+//console.log(lista[2])
+let indice = 0
+let seguirEjecutando = true
+while (indice < 3 ){
+    console.log(lista[indice])
+    indice = indice + 1
+    console.log(lista[indice])
+    indice = indice + 1
+    console.log(lista[indice])
+
+}
