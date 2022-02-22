66ac2258bb23f8a2490f663d88775ce951f07e3f
Alberto21700 <alberto_moreno11@hotmail.es>
2022-02-22 19:57:01 +0100

    añadir for y recorrer el array

--- js/arrays.js
+++ js/arrays.js
@@ -5,12 +5,13 @@ var lista = ["fresa","chocolate","vainilla"]
 // console.log(lista[1])
 // console.log(lista[2])
 var indice = 0
-var seguirEjecutando = true 
-while (indice < 3){
+while (indice < lista.length){
     console.log(lista[indice])
-    indice = indice + 1
-    console.log(lista[indice])
-    indice = indice + 1
-    console.log(lista[indice])
+    indice += 1
     
 }
+
+for (var indice2 = 0; indice2 < lista.length; indice2 += 1) {
+    console.log(lista[indice2])
+}
+
