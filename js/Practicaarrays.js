55516297db1c931295dd70417f7a27542660ee75
sewy210 <96600134+sewy210@users.noreply.github.com>
2022-02-24 21:30:52 +0100

    Terminación de la explicacion final del boss final del final

--- js/Practicaarrays.js
+++ js/Practicaarrays.js
@@ -5,7 +5,18 @@ var lista = [100,200,300]
 //   console.log(lista[indice])
  //   indice -= 1
 //}
-
-for (let i = lista.length; i > 0; i--){
-    console.log(lista[i-1])
+// cojer elemento de una lista
+// lista[3]
+//obtener el indice del ultimo elemento de una lista
+// lista.length - 1
+//si nuentra lista tiene 3 elementos el ultimo indice es 2
+// lista[2] así cojeriamos el ultimo elemento
+let r = lista[lista.length - 1]
+for (let i = lista.length ; i > 1; i--){
+    r -= (lista[i-2])
+   console.log(r)
 }
+console.log(r)
+// 300
+ // -200
+// -100
---
+++ js/akjsia.js
@@ -0,0 +1,12 @@
+var lista = [100,200,300]
+
+// cojer elemento de una lista
+// lista[3]
+//obtener el indice del ultimo elemento de una lista
+// lista.length - 1
+//si nuentra lista tiene 3 elementos el ultimo indice es 2
+// lista[2] así cojeriamos el ultimo elemento
+console.log(lista.length - 1)
+let indice = lista.length - 1
+console.log(indice)
+console.log(lista[lista.length - 1])
