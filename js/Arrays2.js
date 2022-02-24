401b2c36ea7b84afeadc49d7a6e9285a85e9ab12
sewy210 <96600134+sewy210@users.noreply.github.com>
2022-02-24 20:28:04 +0100

    Practica de for

--- js/Arrays2.js
+++ js/Arrays2.js
@@ -9,9 +9,23 @@ var indice = 0
 //resultados += lista[4]
 //console.log(resultados)
  
-while(indice < lista.length){        
-    resultados += lista[indice]
+//while(indice < lista.length){        
+    //resultados += lista[indice]
+    //console.log("vuelta")
+    //indice += 1
+//}
+//console.log(resultados)
+
+
+for(let i= 0; i<lista.length; i++) {
+    resultados += lista[i]
     console.log("vuelta")
-    indice += 1
 }
 console.log(resultados)
+
+// Es una forma de recorrer una list sin usar el bucle "for"
+//var r = 0
+//lista.forEach(elemento => {
+//    r += elemento
+//}) 
+//console.log(r)
---
+++ js/Practicaarrays.js
@@ -0,0 +1,11 @@
+var lista = [100,200,300]
+//var indice = lista.length
+
+//while (indice > 0){
+//   console.log(lista[indice])
+ //   indice -= 1
+//}
+
+for (let i = lista.length; i > 0; i--){
+    console.log(lista[i-1])
+}
