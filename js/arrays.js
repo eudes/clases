3e7fabe96c4180deb2010df638db67d581bb83cb
Alberto21700 <alberto_moreno11@hotmail.es>
2022-02-25 19:20:23 +0100

    actualizar arrays 2

--- js/arrays.js
+++ js/arrays.js
@@ -1,17 +1,15 @@
-var lista = ["fresa","chocolate","vainilla"]
-// console.log(lista)
-// console.log(lista.length)
-// console.log(lista[0])
-// console.log(lista[1])
-// console.log(lista[2])
-var indice = 0
-while (indice < lista.length){
+var lista = ["fresa","chocolate","vainilla", "menta", "limon"]
+// 
+//console.log(lista)
+//console.log(lista.length)
+//console.log(lista[0])
+//console.log(lista[1])
+//console.log(lista[2])
+let indice = 0
+while (indice < lista.length ){
     console.log(lista[indice])
+    // Es lo mismo indice = indice + 1 que  indice += 1
     indice += 1
-    
-}
 
-for (var indice2 = 0; indice2 < lista.length; indice2 += 1) {
-    console.log(lista[indice2])
 }
 
